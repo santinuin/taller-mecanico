8514abd43374beda8b90bcec5e34f32c404d41b1
Santiago Nuñez Ingas <santiago.nunez.ingas@gmail.com>
2023-03-20 20:42:16 -0300

    LoadData methods

--- src/test/java/com/besysoft/taller_mecanico/data/LoadData.java
+++ src/test/java/com/besysoft/taller_mecanico/data/LoadData.java
@@ -11,28 +11,28 @@ import java.util.Optional;
 public class LoadData {
 
     /* Crear vehiculo */
-    Optional<Vehiculo> crearVehiculo() {
+    public static Optional<Vehiculo> crearVehiculo() {
         return Optional.of(new Vehiculo(1L, 2012, "Rojo", "Fiat", "Uno", "AAA111", null));
     }
 
     /* Crear cliente */
-    Optional<Cliente> crearCliente() {
+    public static Optional<Cliente> crearCliente() {
         return Optional.of(new Cliente(1L, "Perez", "111111111", "Calle", "1111", "Departamento", "Localidad", "11", "1", "juanperez@prueba.com", "Juan", "1111111", null));
     }
 
     /* Crear empleado */
 
-    Optional<Empleado> crearEmpleadoRecep() {
+    public static Optional<Empleado> crearEmpleadoRecep() {
         return Optional.of(new Empleado(1L, "Nuñez", "155129748", "Calasanz", "5107", "Colon", "Córdoba", "2770", "1", "Santiago", TipoEmpleadoEnum.RECEPCIONISTA));
     }
 
-    Optional<Empleado> crearEmpleadoAdmin() {
+    public static Optional<Empleado> crearEmpleadoAdmin() {
         return Optional.of(new Empleado(2L, "Mendoza", "156158749", "Peatonal", "5000", "Capital", "Cordoba", "1550", "1", "Carlos", TipoEmpleadoEnum.ADMINISTRATIVO));
     }
 
     /* Crear mecanico */
 
-    Optional<Mecanico> crearMecanico() {
+    public static Optional<Mecanico> crearMecanico() {
         return Optional.of(new Mecanico(1L, 'A', "Gomez", "156894854", "Publica", "5000", "Capital", "Córdoba", "35", "1", "Generalista", "Alberto"));
     }
 
